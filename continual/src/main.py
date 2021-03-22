f38f4971aca9604c3e08d9c071d6728799ba316e
Bhavika Devnani <bhavika.devnani@gmail.com>
2021-03-22 19:06:56 -0400

    Fix comment

--- continual/src/main.py
+++ continual/src/main.py
@@ -1,7 +1,4 @@
-# Copyright (c) Facebook, Inc. and its affiliates. All Rights Reserved
-#
-# This source code is licensed under the license found in the
-# LICENSE file in the root directory of this source tree.
+# Inspired by https://github.com/facebookresearch/Adversarial-Continual-Learning
 
 import os, argparse, time
 import numpy as np
